dcf9be257eba112a8d247dfe1b73bc2a6c5acf90
faresmnasri1990 <41087376+faresmnasri1990@users.noreply.github.com>
2018-07-11 04:02:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import re
+from typing import List
+
+
+client = commands.Bot(command_prefix = ".")
+
+@client.event
+async def on_ready():
+   print("Bot Opend")
+   
+ 
+ 
+ 
+ 
+client.run("NDY2MTQ0MTg4OTAwMDQ4OTA2.DiXzfg.aazCGgSM5E1_C3fEBoCZY8FKoRY")
